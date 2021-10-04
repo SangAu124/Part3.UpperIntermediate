288bbb450c7515cd29d02f6ff926db0f1de30029
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-10-04 23:45:14 +0900

    Update: UpatePracticeOutstargram
    SwiftUI를 사용하기 위해 SnapKit을 설치하고 기초적인 작업을 완료 했습니다.

---
+++ Outstargram/Outstargram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outstargram
+//
+//  Created by DGSW on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
